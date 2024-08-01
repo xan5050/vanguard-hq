f579fcf1220ff998d5082d159a1d19b2f8a17401
Cory Johnson <cory.johnson2911@gmail.com>
2024-07-31 22:02:33 -0400

    Start

--- app.py
+++ app.py
@@ -4,4 +4,32 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
+    super_name = another_method("Camden")
     return "<p>Hello, World!</p>"
+    
+
+def another_method(name):
+    name = "super" + name
+    return name
+
+@app.route("/bob", methods = ['POST'])
+def bob():
+    return "bob"
+
+
+@app.route("/<name>")
+def super_name(name):   
+    name = another_method(name)
+    return name
+
+@app.route("/link") 
+def links():
+    list1 = url_for(super_name)
+    return list1
+
+
+
+if __name__ == "__main__":
+    app.run()
+
+
